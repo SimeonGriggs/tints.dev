eb6632bf3eea24cbde047271513f793ec0151add
Kevin Kirchner <kirchner.kevin@gmail.com>
2020-09-11 13:31:18 -0500

    adding some credit to True Frontier

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,6 +10,14 @@ const Footer = () => (
       >
         simeonGriggs
       </a>{' '}
+      &amp;{' '}
+      <a
+        href="https://truefrontierapps.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        True Frontier
+      </a>{' '}
       |{' '}
       <a
         href="https://github.com/SimeonGriggs/tailwind-css-palette-generator"
