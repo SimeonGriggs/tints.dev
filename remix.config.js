d7945dc8bbd902204abd11221ef04120f937caac
Simeon Griggs <simeon@hey.com>
2022-12-06 18:16:25 +0000

    update remix config

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   serverDependenciesToBundle: ['nanoid'],
+  future: {},
   // appDirectory: 'app',
   // browserBuildDirectory: 'public/build',
   // publicPath: '/build/',
