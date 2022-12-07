93680802b2fc3319578fdacc4cd5385e358a4184
Simeon Griggs <simeon@hey.com>
2022-12-07 13:43:02 +0000

    attempt web vitals

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,9 @@
 import {RemixBrowser} from '@remix-run/react'
 import {hydrateRoot} from 'react-dom/client'
 
+import reportWebVitals from './reportWebVitals'
+import {sendToVercelAnalytics} from './vitals'
+
 hydrateRoot(document, <RemixBrowser />)
+
+reportWebVitals(sendToVercelAnalytics)
