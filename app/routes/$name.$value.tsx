f48a5da8cbe95cb4b91f960557224ea8de969b0d
SimeonGriggs <simeon@hey.com>
2025-06-12 08:37:32 +0100

    ignore assets dir

--- app/routes/$name.$value.tsx
+++ app/routes/$name.$value.tsx
@@ -14,28 +14,33 @@ import {
 import type { Route } from "./+types/$name.$value";
 
 export const loader = async ({ params }: Route.LoaderArgs) => {
-  if (!params?.name) {
-    throw new Response(`No Color Name`, {
-      status: 404,
-      statusText: `Link structure must be ${META.origin}/:name/:value`,
-    });
-  } else if (!params?.value) {
-    throw new Response(`No Hex Value`, {
-      status: 404,
-      statusText: `Link structure must be ${META.origin}/:name/:value`,
-    });
-  } else if (!isValidName(params?.name)) {
-    throw new Response(`Invalid Color Name`, {
-      status: 406,
-      statusText:
-        "Color names must only use lower and uppercase letters, between 3-24 characters",
-    });
-  } else if (!isHex(params?.value)) {
-    throw new Response(`Invalid Hex Value`, {
-      status: 406,
-      statusText:
-        "Color must be a valid hexadecimal value, six characters long, without a leading #",
-    });
+  // In production, the assets directory was running this loader
+  const isServingFromAssets = params?.name === "assets";
+
+  if (!isServingFromAssets) {
+    if (!params?.name) {
+      throw new Response(`No Color Name`, {
+        status: 404,
+        statusText: `Link structure must be ${META.origin}/:name/:value`,
+      });
+    } else if (!params?.value) {
+      throw new Response(`No Hex Value`, {
+        status: 404,
+        statusText: `Link structure must be ${META.origin}/:name/:value`,
+      });
+    } else if (!isValidName(params?.name)) {
+      throw new Response(`Invalid Color Name`, {
+        status: 406,
+        statusText:
+          "Color names must only use lower and uppercase letters, between 3-24 characters",
+      });
+    } else if (!isHex(params?.value)) {
+      throw new Response(`Invalid Hex Value`, {
+        status: 406,
+        statusText:
+          "Color must be a valid hexadecimal value, six characters long, without a leading #",
+      });
+    }
   }
 
   const palette = createPaletteFromNameValue(params.name, params.value);
